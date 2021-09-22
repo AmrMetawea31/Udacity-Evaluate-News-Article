e507daa258ddc72b34874178c6c7dd60a232903d
AmrMetawea31 <76723688+AmrMetawea31@users.noreply.github.com>
2021-09-22 21:09:45 +0200

    Update checkURL.js

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,11 +1,9 @@
 
     function checkURL(value)
 {    var regex = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(regex == null){
-        return false;
-    } else{
-    return true;
-    }
+    if(regex != null){
+        return regex;
+    } 
 }
 
 export { checkURL }
