ac7f477c54db2a9535b2b31369a05411d20c4824
AmrMetawea31 <76723688+AmrMetawea31@users.noreply.github.com>
2021-09-22 21:10:29 +0200

    Update checkURL.js

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,8 +1,8 @@
 
     function checkURL(value)
-{    var regex = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(regex != null){
-        return regex;
+{    var regexp = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if(regexp != null){
+        return regexp;
     } 
 }
 
