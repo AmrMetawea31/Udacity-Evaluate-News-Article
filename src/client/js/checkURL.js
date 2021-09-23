fb0a1b833cdbcadae6c6f03ee5054b93821ceb87
AmrMetawea31 <76723688+AmrMetawea31@users.noreply.github.com>
2021-09-23 09:40:00 +0200

    Update checkURL.js

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,9 +1,11 @@
 
     function checkURL(value)
 {    var regexp = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if(regexp != null){
-        return regexp;
-    } 
+    if(regexp == null){
+        return false;
+    }else{
+        return true;
+    }
 }
 
 export { checkURL }
